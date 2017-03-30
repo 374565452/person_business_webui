e551a1f83484fda375bbb1b357b820c8ae36b649
374565452 <kong42315@163.com>
2017-03-30 17:15:26 +0800

    @2017-3-30 17：13 common-platform webUI前端界面制作，由之前的beyond框架换成zui，并且在下午的时候学习zTree树构件学习

---
+++ beyond_admin/beyond_admin_bak/assets/js/fuelux/treeview/treeview-init.js
@@ -0,0 +1,47 @@
+﻿var UITree = function () {
+
+    return {
+        //main function to initiate the module
+        init: function () {
+
+            var DataSourceTree = function (options) {
+                this._data = options.data;
+                this._delay = options.delay;
+            };
+
+            DataSourceTree.prototype = {
+
+                data: function (options, callback) {
+                    var self = this;
+
+                    setTimeout(function () {
+                        var data = $.extend(true, [], self._data);
+
+                        callback({ data: data });
+                        alert(callback);
+                    }, this._delay)
+                }
+            };
+
+            // INITIALIZING TREE
+            var treeDataSource = new DataSourceTree({
+                data: [
+                    { name: 'Sales', type: 'folder', 'icon-class':'success', additionalParameters: { id: 'F1' } },
+                    { name: 'Projects', type: 'folder', 'icon-class': 'success', additionalParameters: { id: 'F2' } },
+                    { name: 'Reports', type: 'item', additionalParameters: { id: 'I1' } },
+                    { name: 'Finance', type: 'item', additionalParameters: { id: 'I2' } }
+                ],
+                delay: 400
+            });
+
+            $('#MyTree').tree({
+                dataSource: treeDataSource,
+                multiSelect: true,
+                loadingHTML: '<div class="tree-loading"><i class="fa fa-rotate-right fa-spin"></i></div>'
+            });
+
+        }
+
+    };
+
+}();
